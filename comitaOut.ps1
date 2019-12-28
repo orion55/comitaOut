3a348897cb1327c409c9559889c6329be82467b2
Orion55 <grol55@mail.ru>
2019-12-28 18:28:21 +0500

    First commit

---
+++ comitaOut.ps1
@@ -0,0 +1,64 @@
+﻿#Программа для копирования файлов-архивов из системы Comita
+#(c) Гребенёв О.Е. 28.12.2019
+
+[string]$curDir = Split-Path -Path $myInvocation.MyCommand.Path -Parent
+Set-Location $curDir
+[string]$lib = "$curDir\lib"
+
+. $curDir/variables.ps1
+. $lib/PSMultiLog.ps1
+. $lib/libs.ps1
+
+Clear-Host
+
+Start-HostLog -LogLevel Information
+Start-FileLog -LogLevel Information -FilePath $logName -Append
+
+#проверяем существуют ли нужные пути и файлы
+testDir(@($440pArhive, $outcomingPost, $comitaOutPath))
+
+Write-Log -EntryType Information -Message "Начало работы ComitaOut"
+
+if ($debug) {
+	Remove-Item "$comitaOutPath\*.*"
+	Copy-Item -Path "$tmp\work1\*.*" -Destination $comitaOutPath
+}
+
+$arjFiles = Get-ChildItem "$comitaOutPath\$incomingFiles"
+if (($arjFiles | Measure-Object).count -gt 0) {
+	$curDate = Get-Date -Format "ddMMyyyy"
+	$arhivePath = $440pArhive + '\' + $curDate
+	if (!(Test-Path $arhivePath)) {
+		New-Item -ItemType directory -Path $arhivePath | out-Null
+	}
+	$msg = $arjFiles | Copy-Item -Destination $arhivePath -Verbose -Force *>&1
+	Write-Log -EntryType Information -Message ($msg | Out-String)
+
+	$msg = $arjFiles | Move-Item -Destination $outcomingPost -Verbose -Force *>&1
+	Write-Log -EntryType Information -Message ($msg | Out-String)
+
+	$files = Get-ChildItem "$outcomingPost\$incomingFiles"
+
+	$body = "Архив(ы) с отчетностью успешно скопирован(ы) на отправку`n"
+	$msg = $files | ForEach-Object { $_.Name } | Out-String
+	$body += $msg
+	if (($files | Measure-Object).count -gt 0) {
+		if (Test-Connection $mailServer -Quiet -Count 2) {
+			$encoding = [System.Text.Encoding]::UTF8
+			$title = "Архив(ы) на отправке"
+			Send-MailMessage -To $mailAddr -Body $body -Encoding $encoding -From $mailFrom -Subject $title -SmtpServer $mailServer
+		}
+		else {
+			Write-Log -EntryType Error -Message "Не удалось соединиться с почтовым сервером $mailServer"
+		}
+		Write-Log -EntryType Information -Message $body
+	}
+}
+else {
+	Write-Log -EntryType Error -Message "Файлы архивов в каталоге $comitaOutPath не найдены!"
+}
+
+Write-Log -EntryType Information -Message "Конец работы скрипта!"
+
+Stop-FileLog
+Stop-HostLog
---
+++ variables.ps1
@@ -0,0 +1,21 @@
+$curDir = Split-Path -Path $myInvocation.MyCommand.Path -Parent
+
+[string]$tmp = "$curDir\temp"
+[boolean]$debug = $true
+
+#архив
+[string]$440pArhive = "$tmp\440p\Arhive"
+
+#входящие - настройки
+[string]$outcomingPost = "$tmp\Post"
+[string]$incomingFiles = "AFN_7102803_MIFNS00_*_000??.arj"
+[string]$comitaOutPath = "$tmp\comitaOut"
+
+$curDate = Get-Date -Format "ddMMyyyy"
+[string]$logName = $curDir + "\log\" + $curDate + "_comita.log"
+
+#настройка почты
+#[string]$mail_addr = "tmn-f365@tmn.apkbank.apk"
+[string]$mailAddr = "tmn-goe@tmn.apkbank.ru"
+[string]$mailServer = "191.168.6.50"
+[string]$mailFrom = "robot311@tmn.apkbank.apk"
